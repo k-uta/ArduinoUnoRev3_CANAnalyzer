a51762c417d3e12dd00f25e8186c5e5fc9fb0c7d
k-uta <utautau515@gmail.com>
2024-12-27 03:09:00 +0900

    Minor fixes

--- src/main.cpp
+++ src/main.cpp
@@ -13,11 +13,13 @@ MCP_CAN CAN(SPI_CS_PIN); // Set CS pin
         cs -> 10
 */
 
-void setup() {
+void setup()
+{
     Serial.begin(115200);
 
     // Initialize MCP2515
-    while(CAN.begin(MCP_ANY, CAN_1000KBPS, MCP_8MHZ) != CAN_OK) {
+    while (CAN.begin(MCP_ANY, CAN_1000KBPS, MCP_8MHZ) != CAN_OK)
+    {
         Serial.println("Error Initializing MCP2515...");
         delay(1000);
     }
@@ -25,31 +27,36 @@ void setup() {
     CAN.setMode(MCP_NORMAL);
 }
 
-void loop() {
+void loop()
+{
     byte len = 0;
     unsigned char buf[8];
     long unsigned int rxId;
-    if(CAN_MSGAVAIL == CAN.checkReceive()) {
+    if (CAN_MSGAVAIL == CAN.checkReceive())
+    {
         CAN.readMsgBuf(&rxId, &len, buf);
 
-        Serial.print("ID:");
+        Serial.print("time: ");
+        Serial.print(millis());
+        Serial.print("\t");
+        Serial.print("ID:0x");
         Serial.print(rxId, HEX);
-        Serial.print("\t");
-        Serial.print(buf[0]);
-        Serial.print("\t");
-        Serial.print(buf[1]);
-        Serial.print("\t");
-        Serial.print(buf[2]);
-        Serial.print("\t");
-        Serial.print(buf[3]);
-        Serial.print("\t");
-        Serial.print(buf[4]);
-        Serial.print("\t");
-        Serial.print(buf[5]);
-        Serial.print("\t");
-        Serial.print(buf[6]);
-        Serial.print("\t");
-        Serial.print(buf[7]);
+        Serial.print("\t0x");
+        Serial.print(buf[0], HEX);
+        Serial.print("\t0x");
+        Serial.print(buf[1], HEX);
+        Serial.print("\t0x");
+        Serial.print(buf[2], HEX);
+        Serial.print("\t0x");
+        Serial.print(buf[3], HEX);
+        Serial.print("\t0x");
+        Serial.print(buf[4], HEX);
+        Serial.print("\t0x");
+        Serial.print(buf[5], HEX);
+        Serial.print("\t0x");
+        Serial.print(buf[6], HEX);
+        Serial.print("\t0x");
+        Serial.print(buf[7], HEX);
         Serial.print("\n");
     }
 }
